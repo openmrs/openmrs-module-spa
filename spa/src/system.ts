95c1b7a4b50c7fd562d41ef53167d582111840c6
Florian Rappl <florian.rappl@smapiot.com>
2020-05-22 00:45:44 +0200

    Changed with activation function proposal, single file

---
+++ spa/src/system.ts
@@ -0,0 +1,33 @@
+import "import-map-overrides/dist/import-map-overrides";
+import "systemjs/dist/system";
+import "systemjs/dist/extras/amd";
+import "systemjs/dist/extras/named-exports";
+import "systemjs/dist/extras/named-register";
+import "systemjs/dist/extras/use-default";
+
+/**
+ * Gets the loaded import maps from SystemJS.
+ */
+export function getImportMaps() {
+  return Object.getOwnPropertySymbols(System)
+    .map((sym) => System[sym])
+    .filter((m) => typeof m.imports !== "undefined")
+    .map((m) => m.imports)
+    .pop();
+}
+
+/**
+ * Loads all provided modules by their name. Performs a
+ * SystemJS import.
+ * @param modules The names of the modules to resolve.
+ */
+export function loadModules(modules: Array<string>) {
+  return Promise.all(
+    modules.map((name) =>
+      System.import(name).then((value): [string, System.Module] => [
+        name,
+        value,
+      ])
+    )
+  );
+}
