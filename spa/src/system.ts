768ead7f0d019ee4a99783aa0206f120893a570d
Brandon Istenes <bistenes@gmail.com>
2020-09-02 13:53:23 -0700

    Use import map overrides to get import map (#41)

--- spa/src/system.ts
+++ spa/src/system.ts
@@ -6,19 +6,6 @@ import "systemjs/dist/extras/named-register";
 import "systemjs/dist/extras/use-default";
 
 /**
- * Gets the loaded import maps from SystemJS.
- * TODO: Once there is an official API for this we should
- * replace this function with the official API.
- */
-export function getImportMaps() {
-  return Object.getOwnPropertySymbols(System)
-    .map((sym) => System[sym])
-    .filter((m) => typeof m.imports !== "undefined")
-    .map((m) => m.imports)
-    .pop();
-}
-
-/**
  * Loads all provided modules by their name. Performs a
  * SystemJS import.
  * @param modules The names of the modules to resolve.
