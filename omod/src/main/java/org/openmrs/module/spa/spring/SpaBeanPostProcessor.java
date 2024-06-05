b12d2ee145a254e0c9fef4f10295a4348a0c7368
Ian <52504170+ibacher@users.noreply.github.com>
2024-06-05 13:07:13 -0400

    O3-3347: Follow-up; don't use mvc:annotation-driven (#61)

---
+++ omod/src/main/java/org/openmrs/module/spa/spring/SpaBeanPostProcessor.java
@@ -0,0 +1,27 @@
+package org.openmrs.module.spa.spring;
+
+import org.openmrs.module.spa.SpaResourceConverter;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+public class SpaBeanPostProcessor implements BeanPostProcessor, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof RequestMappingHandlerAdapter) {
+            RequestMappingHandlerAdapter handlerMappingAdapter = (RequestMappingHandlerAdapter) bean;
+            handlerMappingAdapter.getMessageConverters().add(0, applicationContext.getBean(SpaResourceConverter.class));
+        }
+        return bean;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
