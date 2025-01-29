3f00d6ad413cd1d0c9d5d091d83dcba87425f956
Ian <52504170+ibacher@users.noreply.github.com>
2025-01-29 10:34:43 -0500

    O3-4395: Fix caching for non-static configuration files (#63)

--- omod/src/main/java/org/openmrs/module/spa/SpaController.java
+++ omod/src/main/java/org/openmrs/module/spa/SpaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,6 +32,10 @@ public class SpaController {
      */
     private static final Pattern URL_PATTERN = Pattern.compile("(?:/ws)?/spa");
 
+    private static final Pattern OPENMRS_CSS_PATTERN = Pattern.compile("openmrs\\.(?:\\w+\\.)?css$");
+
+    private static final MimeType TEXT_JAVASCRIPT = MimeType.valueOf("text/javascript");
+
     private final SpaResourceLoader resourceLoader;
 
     @Autowired
@@ -39,32 +44,8 @@ public class SpaController {
     }
 
     /**
-     * This controller method handles importmap.json, routes.registry.json and service-worker.js, ensuring that they are
-     * sent with no-cache and must-revalidate headers.
-     * <p/>
-     * Note that if you are using the {@code --hash-import-map} feature of the {@code assemble} command, your distro's
-     * importmap will look like importmap.&lt;hash&gt;.json and <em>should</em> be cached.
-     */
-    @RequestMapping("/{filename:(?:importmap|routes\\.registry)\\.json|service-worker\\.js}")
-    public ResponseEntity<Resource> getStaticFileWithoutCacheHeaders(@PathVariable String filename) {
-        FileSystemResource resource = (FileSystemResource) resourceLoader.getResource("/" + filename);
-        if (resource.exists()) {
-            MediaType mediaType = filename.endsWith(".js") ? MediaType.parseMediaType("text/javascript") : MediaType.APPLICATION_JSON;
-
-            return ResponseEntity.ok()
-                    .cacheControl(CacheControl.noCache().mustRevalidate())
-                    .header("Expires", "0")
-                    .contentType(mediaType)
-                    .body(resource);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    /**
-     * This controller method attempts to handle any file with an extension that is not .html or .htm and resolve it to
-     * a file. All resources return in this way should be cached for as long as possible. Technically that's not what
-     * we do here, but 6 months is probably long enough.
+     * This controller method attempts to handle any request for a URL that has a file extension other than .htm or .html.
+     * It attempts to resolve this URL to a file using the resource loader.
      */
     @RequestMapping(value = "/**/{filename:.*\\.(?!html?)[^.]+$}")
     public ResponseEntity<Resource> getStaticFile(HttpServletRequest request) {
@@ -94,22 +75,26 @@ public class SpaController {
                 mediaType = MediaType.APPLICATION_OCTET_STREAM;
             }
 
+            if ((mediaType.includes(TEXT_JAVASCRIPT) && !filename.contains("service-worker")) || OPENMRS_CSS_PATTERN.matcher(filename).matches()) {
+                return ResponseEntity.ok()
+                        .cacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+                        .headers(headers -> headers.setExpires(Instant.now().plus(365, ChronoUnit.DAYS)))
+                        .contentType(mediaType)
+                        .body(resource);
+            }
+
             return ResponseEntity.ok()
-                    .cacheControl(CacheControl.maxAge(180, TimeUnit.DAYS))
-                    .headers(headers ->
-                            headers.setExpires(Instant.now().plus(180, ChronoUnit.DAYS))
-                    )
+                    .cacheControl(CacheControl.noCache().mustRevalidate())
                     .contentType(mediaType)
                     .body(resource);
         } else {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.notFound().cacheControl(CacheControl.noCache().mustRevalidate()).build();
         }
     }
 
     /**
-     * This controller method should handle everything else, and we assume that the correct response is the index.html
-     * file. Like {@link #getStaticFileWithoutCacheHeaders(String)}, this attempts to force the client to always reload
-     * this resource.
+     * This controller method should handle every other URL. We assume that the correct response is the index.html, i.e.,
+     * the single page.
      */
     @RequestMapping({ "/", "/**/{filename:.?(?!.*\\.[^.]*$).*$}", "*.html", "*.htm"})
     public ResponseEntity<Resource> getSinglePage() {
@@ -117,11 +102,10 @@ public class SpaController {
         if (resource.exists()) {
             return ResponseEntity.ok()
                     .cacheControl(CacheControl.noCache().mustRevalidate())
-                    .header("Expires", "0")
                     .contentType(MediaType.TEXT_HTML)
                     .body(resource);
         } else {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.notFound().cacheControl(CacheControl.noCache().mustRevalidate()).build();
         }
     }
 
